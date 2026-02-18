c43c9600fceca89e7e98b4cefb8283edeb05a968
kaustubh-saxena24 <kaustubh24saxena@gmail.com>
2026-02-18 21:50:11 +0530

    Add files via upload

---
+++ chatApp/src/main/java/com/kaustubh/chatApp/config/WebConfig.java
@@ -0,0 +1,19 @@
+package com.kaustubh.chatApp.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**") // Apply to all endpoints
+                .allowedOriginPatterns("*") // Allow ALL origins (localhost, vercel, etc.)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
---
+++ chatApp/src/main/java/com/kaustubh/chatApp/controller/ChatController.java
@@ -0,0 +1,15 @@
+package com.kaustubh.chatApp.controller;
+
+import com.kaustubh.chatApp.model.ChatMessage;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class ChatController {
+    @MessageMapping("/sendMessage")
+    @SendTo("topic/messages")
+    public ChatMessage sendMessage(ChatMessage message){
+        return message;
+    }
+}
