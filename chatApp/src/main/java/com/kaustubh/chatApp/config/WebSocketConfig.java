11441d165c19cbbb6470d727bced23480eae04cd
kaustubh-saxena24 <kaustubh24saxena@gmail.com>
2026-02-18 22:09:15 +0530

    Update WebSocketConfig.java

--- chatApp/src/main/java/com/kaustubh/chatApp/config/WebSocketConfig.java
+++ chatApp/src/main/java/com/kaustubh/chatApp/config/WebSocketConfig.java
@@ -5,13 +5,17 @@ import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-                .setAllowedOrigins("*")
+                // 🔴 DELETE: .setAllowedOrigins("*")
+                // 🟢 ADD THIS INSTEAD:
+                .setAllowedOriginPatterns("*") 
                 .withSockJS();
     }
 
@@ -20,5 +24,4 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
-
 }
